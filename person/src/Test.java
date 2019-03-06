297565679d5ef14ffa66fc068af6bdb0c8421748
RINAX129 <s5.jm2h@gmail.com>
2019-03-07 01:13:51 +0900

    kapuseruka

--- person/src/Test.java
+++ person/src/Test.java
@@ -45,15 +45,25 @@ public class Test{
 
 		//演習⑧
 		Robot aibo =new Robot();
+		aibo.name="aibo";
 		System.out.println(aibo.name);
+		aibo.talk();
+		aibo.walk();
+		aibo.run();
 
 		Robot asimo =new Robot();
+		asimo.name="asimo";
 		System.out.println(asimo.name);
+		asimo.talk();
+		asimo.walk();
+		asimo.run();
 
 		Robot pepper =new Robot();
+		pepper.name="pepper";
 		System.out.println(pepper.name);
-
-
+		pepper.talk();
+		pepper.walk();
+		pepper.run();
 
 	}
 }
