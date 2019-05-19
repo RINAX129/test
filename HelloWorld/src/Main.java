102ae99b76ec3062f1f187dff5396ed7bd1c2280
RINAX129 <s5.jm2h@gmail.com>
2019-05-19 23:32:30 +0900

    Java 配列

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,12 +2,8 @@
 public class Main {
 
 	public static void main(String[] arg){
-		for(int i =1; i<10; i++){
-			for(int j = 1; j<10; j++ ){
-				System.out.print(i*j);
-				System.out.print(" ");
-			}
-			System.out.println("");
-		}
+		int[] a = {1,2,3};
+		a = null;
+		a[0] = 10;
 	}
 }
